a6f5c7926bf6b5773821b2c006ef2fb48cd3f6bc
HayaseYusui <haduki.kana@gmail.com>
2025-05-08 20:19:10 +0900

    原寸ダウンロードを実装

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,32 @@
 <script module lang="ts">
 	import { onMount } from 'svelte';
+	const textBoxClasses =
+		'rounded border border-gray-300 bg-white px-3 py-1 text-base text-gray-700 transition-colors duration-200 ease-in-out outline-none focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200';
+	const buttonClasses =
+		'rounded border-0 bg-indigo-500 px-6 py-1 text-white hover:bg-indigo-600 focus:outline-none';
+	const labelClasses = 'text-sm leading-7 text-gray-600';
 </script>
 
 <script lang="ts">
 	import { Canvas, FabricImage, FabricText } from 'fabric';
-
+	/** 表示用縮尺倍率 */
+	const multiplier = 5;
 	let canvasElement: HTMLCanvasElement;
 	let canvas: Canvas;
 	let charaName: string = $state('');
-	let section: string = $state('');
-	let credential: string = $state('');
-
-	const scaleRatio = 1 / 5;
+	let section: string = $state('公安対魔特務六課');
+	let credential: string = $state('特別捜査官');
 
 	onMount(async () => {
-		const img = await FabricImage.fromURL(
-			'/sho.png',
-			{},
-			{
-				selectable: false
-			}
-		);
+		const img = await FabricImage.fromURL('/sho.png', {}, { selectable: false });
 
-		const imgWidth = img.width * scaleRatio;
-		const imgHeight = img.height * scaleRatio;
-
-		canvasElement.width = imgWidth;
-		canvasElement.height = imgHeight;
-
-		img.scale(scaleRatio); // 画像のスケーリング
+		const scaleWidth = img.width / multiplier;
+		const scaleHeight = img.height / multiplier;
+		// 縮小して表示
+		img.scaleToWidth(scaleWidth);
+		img.scaleToHeight(scaleHeight);
+		canvasElement.width = scaleWidth;
+		canvasElement.height = scaleHeight;
 
 		canvas = new Canvas(canvasElement);
 		canvas.add(img);
@@ -41,35 +39,33 @@
 
 	function addCharName(): void {
 		const textObj = new FabricText(charaName, {
-			left: 320,
+			left: 325,
 			top: 150,
-			fontSize: 22,
+			fontSize: 30,
 			fontWeight: 'bold',
 			fill: 'black'
 		});
 
 		canvas.add(textObj);
-		canvas.setActiveObject(textObj);
 		canvas.renderAll();
 	}
 
 	function addSection(): void {
 		const textObj = new FabricText(section, {
-			left: 320,
-			top: 222,
+			left: 325,
+			top: 224,
 			fontSize: 22,
 			fontWeight: 'bold',
 			fill: 'black'
 		});
 
 		canvas.add(textObj);
-		canvas.setActiveObject(textObj);
 		canvas.renderAll();
 	}
 
 	function addCredential(): void {
 		const textObj = new FabricText(credential, {
-			left: 320,
+			left: 325,
 			top: 297,
 			fontSize: 22,
 			fontWeight: 'bold',
@@ -77,7 +73,6 @@
 		});
 
 		canvas.add(textObj);
-		canvas.setActiveObject(textObj);
 		canvas.renderAll();
 	}
 
@@ -127,7 +122,7 @@
 					top: 116,
 					selectable: true
 				});
-				fabricImg.scale(scaleRatio);
+				fabricImg.scale(1 / multiplier);
 				canvas.add(fabricImg);
 				canvas.sendObjectToBack(fabricImg);
 				canvas.setActiveObject(fabricImg);
@@ -139,8 +134,8 @@
 	}
 
 	function downloadImage(): void {
-		const imageSrc = canvas.toDataURL();
-		// some download code down here
+		const imageSrc = canvas.toDataURL({ multiplier });
+
 		const a = document.createElement('a');
 		a.href = imageSrc;
 		a.download = 'image.png';
@@ -179,12 +174,14 @@
 		rel="stylesheet"
 	/>
 </svelte:head>
-<section class="body-font text-gray-600">
-	<div class="container flex">
-		<div class="flex flex-col rounded-lg bg-white p-2 shadow-md md:m-auto">
+<section class="container max-w-[49rem] p-2">
+	<div class="grid grid-cols-1 gap-2 md:grid-cols-[3rem_auto_10rem] md:gap-4">
+		<div class="col-span-full">
 			<h1 class="title-font mb-1 text-lg font-medium text-gray-900">職員証作成サービス</h1>
-			<p class="mb-5 leading-relaxed text-gray-600">画像に文字入れられます!</p>
-			<div class=" mb-4">
+			<p class="leading-relaxed text-gray-600">職員証に文字を入れてダウンロードできます!</p>
+		</div>
+		<div class="col-span-full overflow-auto md:justify-items-center">
+			<div class="w-full pb-5 md:pb-0">
 				<canvas
 					bind:this={canvasElement}
 					onkeydown={(e) => handleKeyDown(e)}
@@ -192,77 +189,59 @@
 					class="border-1"
 				></canvas>
 			</div>
-			<div class=" mb-4">
-				<label for="charaName" class=" text-sm leading-7 text-gray-600">氏名</label>
-				<input
-					type="text"
-					id="charaName"
-					name="charaName"
-					bind:value={charaName}
-					class="w-[50%] rounded border border-gray-300 bg-white px-3 py-1 text-base text-gray-700 transition-colors duration-200 ease-in-out outline-none focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
-				/>
-				<button
-					onclick={addCharName}
-					class="w-[10rem] rounded border-0 bg-indigo-500 px-6 py-1 text-white hover:bg-indigo-600 focus:outline-none"
-					>氏名を挿入</button
-				>
-			</div>
-			<div class=" mb-4">
-				<label for="section" class=" text-sm leading-7 text-gray-600">部署</label>
-				<input
-					type="text"
-					id="section"
-					name="section"
-					bind:value={section}
-					class="w-[50%] rounded border border-gray-300 bg-white px-3 py-1 text-base text-gray-700 transition-colors duration-200 ease-in-out outline-none focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
-				/>
-				<button
-					onclick={addSection}
-					class="w-[10rem] rounded border-0 bg-indigo-500 px-6 py-1 text-white hover:bg-indigo-600 focus:outline-none"
-					>部署を挿入</button
-				>
-			</div>
-			<div class=" mb-4">
-				<label for="credential" class=" text-sm leading-7 text-gray-600">資格</label>
-				<input
-					type="text"
-					id="credential"
-					name="credential"
-					bind:value={credential}
-					class="w-[50%] rounded border border-gray-300 bg-white px-3 py-1 text-base text-gray-700 transition-colors duration-200 ease-in-out outline-none focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
-				/>
-				<button
-					onclick={addCredential}
-					class="w-[10rem] rounded border-0 bg-indigo-500 px-6 py-1 text-white hover:bg-indigo-600 focus:outline-none"
-					>資格を挿入</button
-				>
-			</div>
-			<div class=" mb-4">
-				<label for="gazo" class="text-sm leading-7 text-gray-600">写真</label>
-				<input
-					type="file"
-					name="gazo"
-					accept="image/*"
-					onchange={handleImageUpload}
-					class="w-[24rem] cursor-pointer rounded border border-gray-300 px-3 text-sm leading-8 focus:outline-none"
-				/>
-				<button
-					onclick={deleteImage}
-					class="w-[10rem] rounded border-0 bg-indigo-500 px-6 py-1 text-white hover:bg-indigo-600 focus:outline-none"
-					>画像を削除</button
-				>
-			</div>
-			<p class="mb-4 text-xs text-gray-500">
-				※画面に挿入したものは選んでデリートキーかバックスペースキーで消せます
-			</p>
-			<div>
-				<button
-					onclick={downloadImage}
-					class="w-[26rem] rounded border-0 bg-indigo-500 px-6 py-1 text-white hover:bg-indigo-600 focus:outline-none"
-					>職員証をダウンロード</button
-				>
-			</div>
 		</div>
+		<label for="charaName" class={`${labelClasses}`}>氏名</label>
+		<input
+			type="text"
+			id="charaName"
+			name="charaName"
+			bind:value={charaName}
+			class={`w-full ${textBoxClasses}`}
+		/>
+		<button onclick={addCharName} class={`w-full ${buttonClasses}`}>氏名を挿入</button>
+		<label for="section" class={`${labelClasses}`}>部署</label>
+		<input
+			type="text"
+			id="section"
+			name="section"
+			bind:value={section}
+			class={`w-full ${textBoxClasses}`}
+		/>
+		<button onclick={addSection} class={buttonClasses}>部署を挿入</button>
+		<label for="credential" class={`${labelClasses}`}>資格</label>
+		<input
+			type="text"
+			id="credential"
+			name="credential"
+			bind:value={credential}
+			class={`w-full ${textBoxClasses}`}
+		/>
+		<button onclick={addCredential} class={`w-full ${buttonClasses}`}>資格を挿入</button>
+		<label for="gazo" class={`${labelClasses}`}>写真</label>
+		<input
+			type="file"
+			id="gazo"
+			name="gazo"
+			accept="image/*"
+			onchange={handleImageUpload}
+			class="w-full cursor-pointer rounded border border-gray-300 px-3 text-sm leading-8 focus:outline-none"
+		/>
+		<button onclick={deleteImage} class={`w-full ${buttonClasses}`}>画像を削除</button>
+		<div class="col-span-full">
+			<p class="text-xs text-gray-500">
+				※画面に挿入したものは選んでデリートキーかバックスペースキーで消せます。挿入した画像は削除ボタンで削除します。
+			</p>
+		</div>
+		<div class="col-span-full md:col-span-2">
+			<button onclick={downloadImage} class={`w-full ${buttonClasses}`}>職員証をダウンロード</button
+			>
+		</div>
+		<button
+			onclick={() => {
+				window.location.reload();
+			}}
+			class={`w-full ${buttonClasses}`}>リセット</button
+		>
 	</div>
 </section>
 
